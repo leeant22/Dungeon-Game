36a74602c181e3330468b423969570cfe96ac9ca
leeant22 <leeant22@uw.edu>
2025-05-29 20:22:00 -0700

    Fixed swinging sound effect and added hit sound for club

--- Assets/Scripts/EnemyWeapon.cs
+++ Assets/Scripts/EnemyWeapon.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class EnemyWeapon : MonoBehaviour
 {
     public EnemyAI enemyAI;
+    public AudioClip hitSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            GetComponent<AudioSource>().PlayOneShot(hitSound);
             enemyAI.Hit();
         }
     }
